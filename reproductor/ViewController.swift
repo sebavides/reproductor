ba898865de4f1873d138957a0668dba59b4fb874
sebavides <sebavides@gmail.com>
2015-12-22 18:43:39 -0300

    Initial Commit

---
+++ reproductor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  reproductor
+//
+//  Created by Seba Benavides on 22-12-15.
+//  Copyright © 2015 Seba Benavides. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
